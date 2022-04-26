455fd4c3ae8038978d7ec46023609ddcb14a0816
cardon temka <cardontemka@LEAP-7s-iMac.local>
2022-04-26 15:17:38 +0800

    4.26

--- Game/ProjectGame.html/projectGame2.js
+++ Game/ProjectGame.html/projectGame2.js
@@ -1,32 +1,39 @@
+let wiw = window.innerWidth;
+let wih = window.innerHeight;
+
+// Canvas
 const canvas = document.querySelector("canvas");
-
-let arrowR = document.getElementById("arrowR");
-
-let arrowL = document.getElementById("arrowL");
-console.log(arrowR, arrowL)
-
-const c = canvas.getContext("2d");
-
-let button = document.getElementById("but");
-
-var pause = true;
-var stop = false;
-const currentHref = window.location.href;
 canvas.width = 420;
 canvas.height = 700;
+const c = canvas.getContext("2d");
 
-var mouseX, mouseY;
-let wiw = window.innerWidth,
-  wih = window.innerHeight;
+// Arrows
+let arrowR = document.getElementById("arrowR");
+let arrowL = document.getElementById("arrowL");
 
-var colors = ["green", "yellow", "orange", "brown", "blue", "grey", "black"];
-canvas.style.width = "420px";
-canvas.style.height = "700px";
+// Score
+let score = document.getElementById("timer");
+let scoreCount = 100;
+
+//Pause Button
+const buttonEl = document.getElementById("button");
+
+// States
+var pause = true;
+var stop = false;
+
+// Drag
+var mouseX;
+var mouseY;
+
+var colors = ["yellow", "brown", "green", "orange", "blue", "grey", "black"];
+
 
 let widthcent = (wiw - canvas.width) / 2;
-let heicent = (wih - canvas.height) / 2;
+let heicent = (wih - canvas.height) / 2; // Nershil!
 
-canvas.style.marginTop = heicent + "px";
+canvas.style.marginTop = heicent + "px"; // CSS bolgo
+
 let drag;
 let mbx, mby;
 let mpx, mpy;
@@ -40,20 +47,23 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 //TrashCans classes
 class Trash {
-  constructor({ position, color }) {
+  constructor({ position, color, id }) {
     this.position = position;
     this.width = 80;
     this.height = 100;
     this.color = color;
+    this.id = id;
   }
   draw() {
     c.fillStyle = this.color;
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+  
 }
+
 //Garbage classes
 class Garbage {
-  constructor({ x, y, velocity, color , id}) {
+  constructor({ x, y, velocity, color, id }) {
     this.x = x;
     this.y = y;
     this.velocity = velocity;
@@ -65,73 +75,35 @@ class Garbage {
   draw() {
     c.fillStyle = this.color;
     c.fillRect(this.x, this.y, this.width, this.height);
-    //console.log(this.color);
   }
   update() {
     this.draw();
     this.velocity.y = gravity;
     this.y += this.velocity.y;
   }
+  detectr() {
+    delete this.x;
+    delete this.y;
+    delete this.width;
+    delete this.height;
+    delete this.color;
+    delete this.id;
+  }
 }
+
 var trashes = [];
-
-trashes.push(
-  new Trash({
-    position: {
-      x: -190,
-      y: 585,
-    },
-    color: "yellow",
-  })
-);
-
-trashes[1] = new Trash({
-  position: {
-    x: -70,
-    y: 585,
-  },
-  color: "brown",
-});
-
-trashes[2] = new Trash({
-  position: {
-    x: 50,
-    y: 585,
-  },
-  color: "green",
-});
-
-trashes[3] = new Trash({
-  position: {
-    x: 170,
-    y: 585,
-  },
-  color: "orange",
-});
-
-trashes[4] = new Trash({
-  position: {
-    x: 290,
-    y: 585,
-  },
-  color: "blue",
-});
-
-trashes[5] = new Trash({
-  position: {
-    x: 410,
-    y: 585,
-  },
-  color: "grey",
-});
-
-trashes[6] = new Trash({
-  position: {
-    x: 530,
-    y: 585,
-  },
-  color: "black",
-});
+for (let i = 0; i < 7; i++) {
+  trashes.push(
+    new Trash({
+      position: {
+        x: -190 + i * 120,
+        y: 585,
+      },
+      color: colors[i],
+      id: i,
+    })
+  );
+}
 
 var cal = canvas.width - 45;
 
@@ -146,6 +118,9 @@ arrowR.style.top = heicent + canvas.height - 80 + "px";
 
 arrowL.style.left = widthcent + 2 + "px";
 arrowL.style.top = heicent + canvas.height - 82 + "px";
+
+buttonEl.style.left = widthcent + 10 + "px";
+buttonEl.style.top = heicent + 10 + "px";
 
 arrowL.addEventListener("click", keyMoveR);
 arrowR.addEventListener("click", keyMoveL);
@@ -171,10 +146,11 @@ const interval = setInterval(function () {
       x: 0,
       y: 1,
     };
-    garbage = new Garbage({ x, y, velocity, color });
+    const id = colors.indexOf(color);
+    garbage = new Garbage({ x, y, velocity, color, id });
     garbages.push(garbage);
   }
-}, 2000);
+}, 1800);
 
 // Mouse down
 
@@ -252,16 +228,19 @@ function drawer() {
   canvas.style.backgroundColor = "white";
 }
 let garbageNull;
+
+// Main Animate Function
+
 function animate() {
   window.requestAnimationFrame(animate);
   dreg();
-  wiw = innerWidth;
-  widthcent = (wiw - canvas.width) / 2;
-  heicent = (wih - canvas.height) / 2;
-  arrowR.style.left = widthcent + canvas.width - 40 + "px";
-  arrowR.style.top = heicent + canvas.height - 80 + "px";
-  arrowL.style.left = widthcent + 2 + "px";
-  arrowL.style.top = heicent + canvas.height - 82 + "px";
+  // wiw = innerWidth;
+  // widthcent = (wiw - canvas.width) / 2;
+  // heicent = (wih - canvas.height) / 2;
+  // arrowR.style.left = widthcent + canvas.width - 40 + "px";
+  // arrowR.style.top = heicent + canvas.height - 80 + "px";
+  // arrowL.style.left = widthcent + 2 + "px";
+  // arrowL.style.top = heicent + canvas.height - 82 + "px";
 
   if (pause) {
     detect();
@@ -269,9 +248,12 @@ function animate() {
     garbages.forEach((garbage) => {
       garbage.update();
     });
+    score.innerText = scoreCount;
+    scoreMine();
   }
   drawer();
 }
+
 animate();
 
 window.addEventListener("focus", () => {
@@ -286,6 +268,7 @@ window.addEventListener("blur", () => {
 
 let timer = null;
 let timerId;
+
 // function decreaseTimer() {
 //   if (timer < 10000) {
 //     timerId = setTimeout(decreaseTimer, 100);
@@ -307,14 +290,13 @@ function keyMoveL() {
         trashes[0].position.x -= 840;
       }
     }
-
   }, 30);
 
   if (tlmove >= 12) {
     tlmove = 0;
     clearInterval(interval);
   }
-  console.log(trashes);
+  // console.log(trashes);
 }
 
 function keyMoveR() {
@@ -335,15 +317,14 @@ function keyMoveR() {
     trmove = 0;
     clearInterval(interval);
   }
-
-  console.log(trashes);
+  // console.log(trashes);
 }
 
 let yi;
 
 garbageNull = new Garbage({
-  x: Math.floor(Math.random() * cal),
-  y: -80,
+  x: 0,
+  y: -100,
   color: "white",
   velocity: {
     x: 0,
@@ -357,41 +338,158 @@ function detect() {
       garbage.y > 585 &&
       garbage.y < 685 &&
       garbage.x > 40 &&
-      garbage.x < 130
+      garbage.x < 120
     ) {
       setTimeout(() => {
-        if (garbages[index] == garbages[idx]) {
-          drag = false;
+        if (trashes[2].id == garbage.id) {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount += 20;
+        } else {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          // garbages.splice(index, 1, garbageNull);
+          garbage.detectr();
+          scoreCount -= 20;
+          console.log(101)
         }
-        garbages.splice(index, 1, garbageNull);
       }, 0);
     }
     if (
       garbage.y > 585 &&
       garbage.y < 685 &&
       garbage.x > 160 &&
-      garbage.x < 260
+      garbage.x < 240
     ) {
       setTimeout(() => {
-        if (garbages[index] == garbages[idx]) {
-          drag = false;
+        if (trashes[3].id == garbage.id) {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount += 20;
+        } else {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount -= 20;
+          console.log(202)
         }
-        garbages.splice(index, 1, garbageNull);
       }, 0);
     }
     if (
       garbage.y > 585 &&
       garbage.y < 685 &&
       garbage.x > 280 &&
-      garbage.x < 380
+      garbage.x < 360
     ) {
       setTimeout(() => {
-        if (garbages[index] == garbages[idx]) {
-          drag = false;
+        if (trashes[4].id == garbage.id) {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount += 20;
+        } else {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount -= 20;
+          console.log(303)
         }
-        garbages.splice(index, 1, garbageNull);
+      }, 0);
+    }
+
+    //suuliin 2
+    if (
+      garbage.y > 585 &&
+      garbage.y < 685 &&
+      garbage.x > 400 &&
+      garbage.x < 480
+    ) {
+      setTimeout(() => {
+        if (trashes[5].id == garbage.id) {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount += 20;
+        } else {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount -= 20;
+          console.log(404)
+        }
+      }, 0);
+    }
+    if (
+      garbage.y > 585 &&
+      garbage.y < 685 &&
+      garbage.x > -60 &&
+      garbage.x < 5
+    ) {
+      setTimeout(() => {
+        if (trashes[1].id == garbage.id) {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount += 20;
+        } else {
+          if (garbages[index] == garbages[idx]) {
+            drag = false;
+          }
+          garbage.detectr();
+          // garbages.splice(index, 1, garbageNull);
+          scoreCount -= 20;
+          console.log(505)
+        }
       }, 0);
     }
   });
 }
-console.log(trashes);
+
+function scoreMine() {
+  garbages.forEach((garbage) => {
+    if (garbage.y == canvas.height) {
+      garbage.detectr();
+      scoreCount -= 25;
+      console.log(666)
+    }
+    // if (scoreCount <= 0) {
+    //   // alert("You Lose");
+    //   // scoreCount = 100;
+    // }
+    // if (scoreCount >= 1000) {
+    //   // alert("You won");
+    //   // scoreCount = 100;
+    // }
+  });
+}
+
+function resizer() {
+  wiw = innerWidth;
+  widthcent = (wiw - canvas.width) / 2;
+  heicent = (wih - canvas.height) / 2;
+  arrowR.style.left = widthcent + canvas.width - 40 + "px";
+  arrowR.style.top = heicent + canvas.height - 80 + "px";
+  arrowL.style.left = widthcent + 2 + "px";
+  arrowL.style.top = heicent + canvas.height - 82 + "px";
+  buttonEl.style.left = widthcent + 10 + "px";
+  buttonEl.style.top = heicent + 10 + "px";
+}
