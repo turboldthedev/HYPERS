85b0de0078a75b4a267aaabb06d9c80b3d684a1a
Altan-Od <altan-odb@Khulangoos-iMac-2.local>
2022-04-26 18:04:19 +0800

    4:26-18:04

---
+++ QUIZGAME/countryside/country-side-aqg.js
@@ -0,0 +1,6 @@
+var questionBar = document.getElementById("question");
+var clickAfter = document. getElementById("nextclicker")
+function nextclick() {
+    questionBar.style.display = ;
+}
+document.addEventListener('mousedown', nextclick()); 
