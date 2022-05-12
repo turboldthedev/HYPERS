ab3f4d328fd2f0208b1b0d49240cbfb04256ee96
tushig <tushig.b@Bilguuns-iMac.local>
2022-05-12 17:59:39 +0800

    5.12

--- public/Quiz starting/Demoday/starting.js
+++ public/Quiz starting/Demoday/starting.js
@@ -11,3 +11,27 @@ function startingMap() {
   containerEl.classList.add("appear");
   console.log(textEl);
 }
+
+console.log(screen.orientation.type);
+
+const cont = document.getElementById('cont');
+const plsrotate = document.getElementById('rotate');
+console.log(cont);
+
+if (screen.orientation.type == 'portrait-primary' ) { 
+  console.log("huurhun");
+ cont.classList.add("invisible");
+}
+
+addEventListener('orientationchange', event => { 
+  console.log(screen.orientation.type);
+  if (screen.orientation.type == 'landscape-primary') { 
+   cont.classList.remove("invisible");
+   plsrotate.classList.add("invisible")
+  }
+  else { 
+    cont.classList.add("invisible");
+    plsrotate.classList.remove("invisible")
+  }
+});
+
